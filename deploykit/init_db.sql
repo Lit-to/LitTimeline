89b848cc4b0f150ea4bafdeec36db8ffbc047ee6
Lit-to <litto0315@gmail.com>
2025-02-23 13:13:41 +0900

    update: データベース初期化とユーザ権限テスト用のSQLスクリプトを移動

---
+++ deploykit/init_db.sql
@@ -0,0 +1,11 @@
+
+-- ユーザの作成 ※パスワード設定は必要に応じてこの処理のあとに変更する。
+SET GLOBAL validate_password.policy = 'LOW';
+ALTER USER root@localhost IDENTIFIED BY 'password';
+CREATE USER api@localhost IDENTIFIED BY 'password';
+CREATE USER dev@localhost IDENTIFIED BY 'password';
+FLUSH PRIVILEGES;
+
+-- 権限の付与
+GRANT SELECT,INSERT,UPDATE ON litter.* TO 'api'@'localhost';
+GRANT SELECT,INSERT,UPDATE,DELETE,ALTER,CREATE ON litter.* TO 'dev'@'localhost';
