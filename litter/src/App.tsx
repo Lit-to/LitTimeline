cdcabbe293b99b178534927c5fda88eafd42aed2
Lit-to <litto0315@gmail.com>
2025-03-01 06:42:31 +0000

    add: react-router-domを追加し、Appを仮サイトマップにした

--- litter/src/App.tsx
+++ litter/src/App.tsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+    const title: string = "Litter";
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <div className="App">
+            <h1>{title}</h1>
+            <h2>今のところただのテストぺージ。とりあえず作ったページをならべるよ</h2>
+            <li><a href="./account">アカウントを作る</a></li> 
+            <li>ホーム？</li> 
+        </div>
+    );
 }
 
-export default App
