8cb5cc5d8ed5c5ecfa44eb9c5e71dcf3d2a008c5
Lit-to <litto0315@gmail.com>
2025-03-01 11:04:16 +0000

    add: Accountコンポーネントを作成

---
+++ litter/src/Account.tsx
@@ -0,0 +1,13 @@
+// import "./index.css"
+
+export const Acount = () => {
+    const title: string = "アカウントを作る";
+    const p_id: string = "id:";
+    const p_password: string = "パスワード:";
+
+    return (
+        <div className="Test">
+            <h1>{title}</h1>
+        </div>
+    );
+}
