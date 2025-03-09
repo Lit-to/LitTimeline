451b08377e1e94b32707ce72b98fac5cf1db2035
Lit-to <litto0315@gmail.com>
2025-03-09 15:29:58 +0000

    update: レイアウトとスタイルを改善し、モーダルの構造を変更

--- litter/src/signup/modal.tsx
+++ litter/src/signup/modal.tsx
@@ -1,11 +1,12 @@
-import Modal from 'react-bootstrap/Modal';
+// import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
 import styles from "./modal.module.css";
+import temp_screenshot from "../assets/computer_internet_enjou.png";
 
 export const Signup = () => {
     const [isLogin, setIsLogin] = useState(true);
     const [isExhibited, setShow] = useState(true);
-
+    const name: string = "Litter";
     const title: string = isLogin ? "アカウントを作る" : "ログイン";
     const p_id: string = "ID : ";
     const p_password: string = "パスワード : ";
@@ -15,38 +16,35 @@ export const Signup = () => {
     const change_button: string = isLogin ? "ログインする" : "アカウントを作る";
 
     const handleLogin = () => {
-        setIsLogin(!isLogin);
+        setIsLogin(!isLogin)
     }
 
 
     return (
         <div className={styles.root}>
-            <span>
-                <Modal.Dialog>
-                    <Modal.Header>
-                        <Modal.Title>{title}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <form>
-                            <label>
-                                {p_id}
-                                <input type="text" name="ID" />
-                            </label>
-                            <label>
-                                {p_password}
-                                <input type="password" name="password" />
-                            </label>
-                            <a href={link} target='about:blank'>
-                                {excuse}
-                            </a>
-                            <div className='button'>
-                                <button >{enter_button}</button>
-                                <button onClick={handleLogin}> {change_button} </button>
-                            </div>
-                        </form>
-                    </Modal.Body>
-                </Modal.Dialog>
-            </span>
+            <div className={styles.vertical}>
+                <h1>{name}</h1>
+                <h2>{title}</h2>
+                <form>
+                    <label>
+                        {p_id}
+                        <input type="text" name="ID" />
+                    </label>
+                    <label>
+                        {p_password}
+                        <input type="password" name="password" />
+                    </label>
+                    <a href={link} target='about:blank'>
+                        {excuse}
+                    </a>
+                    <div className={styles.horizontal}>
+                    <button type="button" >{enter_button}</button>
+                    <button type="button" onClick={handleLogin}> {change_button} </button>
+                    </div>
+                </form>
+            </div>
+            <img src={temp_screenshot} className={styles.screenshot}></img>
+
 
         </div>
 
