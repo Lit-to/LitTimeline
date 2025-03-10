111df3cd3a61e87d84817d3d76204428db20a91b
Lit-to <litto0315@gmail.com>
2025-03-10 15:06:21 +0000

    update: モーダルのスタイルを改善し、レスポンシブデザインを追加

--- litter/src/signup/modal.tsx
+++ litter/src/signup/modal.tsx
@@ -6,7 +6,7 @@ import temp_screenshot from "../assets/computer_internet_enjou.png";
 export const Signup = () => {
     const [isLogin, setIsLogin] = useState(true);
     const [isExhibited, setShow] = useState(true);
-    const name: string = "Litter";
+    const name: string = "Tlitter";
     const title: string = isLogin ? "アカウントを作る" : "ログイン";
     const p_id: string = "ID : ";
     const p_password: string = "パスワード : ";
@@ -26,24 +26,28 @@ export const Signup = () => {
                 <h1>{name}</h1>
                 <h2>{title}</h2>
                 <form>
-                    <label>
-                        {p_id}
-                        <input type="text" name="ID" />
-                    </label>
-                    <label>
-                        {p_password}
-                        <input type="password" name="password" />
-                    </label>
-                    <a href={link} target='about:blank'>
-                        {excuse}
-                    </a>
+                    <div className={styles.input_area}>
+                        <label>
+                            {p_id}
+                        </label>
+                        <label>
+                            <input type="text" name="ID" />
+                        </label>
+                        <label>
+                            {p_password}</label>
+                        <label>
+                            <input type="password" name="password" />
+                        </label>
+                        <a href={link} target='about:blank'>
+                            {excuse}
+                        </a>
+                    </div>
                     <div className={styles.horizontal}>
-                    <button type="button" >{enter_button}</button>
-                    <button type="button" onClick={handleLogin}> {change_button} </button>
+                        <button type="button" >{enter_button}</button>
+                        <button type="button" onClick={handleLogin}> {change_button} </button>
                     </div>
                 </form>
             </div>
-            <img src={temp_screenshot} className={styles.screenshot}></img>
 
 
         </div>
