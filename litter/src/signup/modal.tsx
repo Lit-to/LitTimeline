ba54e734733fc683ba5d6f5959d393a4cb22398f
Lit-to <litto0315@gmail.com>
2025-03-11 13:42:10 +0000

    update: すまほがめんが綺麗になるように調整

--- litter/src/signup/modal.tsx
+++ litter/src/signup/modal.tsx
@@ -1,16 +1,14 @@
 // import Modal from 'react-bootstrap/Modal';
 import { useState } from 'react';
 import styles from "./modal.module.css";
-import temp_screenshot from "../assets/computer_internet_enjou.png";
 
 export const Signup = () => {
     const [isLogin, setIsLogin] = useState(true);
-    const [isExhibited, setShow] = useState(true);
     const name: string = "Tlitter";
     const title: string = isLogin ? "アカウントを作る" : "ログイン";
     const p_id: string = "ID : ";
     const p_password: string = "パスワード : ";
-    const excuse: string = isLogin ? "※パスワードはがんばれば全世界からばれるかも！" : "パスワードを忘れた";
+    const excuse: string = isLogin ? "パスワード大公開宣言" : "パスワードを忘れた";
     const link: string = isLogin ? "https://www.soumu.go.jp/main_sosiki/cybersecurity/kokumin/security/business/staff/06/" : "https://www.soumu.go.jp/main_sosiki/cybersecurity/kokumin/security/business/staff/06/";
     const enter_button: string = isLogin ? "SignUp" : "Login";
     const change_button: string = isLogin ? "ログインする" : "アカウントを作る";
@@ -38,10 +36,13 @@ export const Signup = () => {
                         <label>
                             <input type="password" name="password" />
                         </label>
+                    </div>
+                    <label>
                         <a href={link} target='about:blank'>
                             {excuse}
                         </a>
-                    </div>
+                        </label>
+
                     <div className={styles.horizontal}>
                         <button type="button" >{enter_button}</button>
                         <button type="button" onClick={handleLogin}> {change_button} </button>
