832a516ff0b34822b8741d351c34da31936d49a7
Lit-to <litto0315@gmail.com>
2025-03-23 15:49:44 +0000

    feat: MySQLのユーザー作成と権限付与のIPアドレスを変更し、より広範な接続を許可

--- deploykit/db/sql/0_init_db.sql
+++ deploykit/db/sql/0_init_db.sql
@@ -4,14 +4,13 @@ SET GLOBAL validate_password_policy = 'LOW';
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
 CREATE USER 'api'@'localhost' IDENTIFIED BY 'password';
 CREATE USER 'dev'@'localhost' IDENTIFIED BY 'password';
--- CREATE USER 'root'@'172.19.0.%' IDENTIFIED BY 'password';
-CREATE USER 'api'@'172.19.0.%' IDENTIFIED BY 'password';
-CREATE USER 'dev'@'172.19.0.%' IDENTIFIED BY 'password';
+CREATE USER 'api'@'172.%' IDENTIFIED BY 'password';
+CREATE USER 'dev'@'172.%' IDENTIFIED BY 'password';
 
 -- 権限の付与
 GRANT SELECT,INSERT,UPDATE ON litter.* TO 'api'@'localhost';
-GRANT SELECT,INSERT,UPDATE ON litter.* TO 'api'@'172.19.0.%';
+GRANT SELECT,INSERT,UPDATE ON litter.* TO 'api'@'172.%';
 GRANT SELECT,INSERT,UPDATE,DELETE,ALTER,CREATE ON litter.* TO 'dev'@'localhost';
-GRANT SELECT,INSERT,UPDATE,DELETE,ALTER,CREATE ON litter.* TO 'dev'@'172.19.0.%';
+GRANT SELECT,INSERT,UPDATE,DELETE,ALTER,CREATE ON litter.* TO 'dev'@'172.%';
 
 FLUSH PRIVILEGES;
