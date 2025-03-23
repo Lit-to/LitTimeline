54c58295cd59e62eaea2ae3ec9e1d6ece2715b12
Lit-to <litto0315@gmail.com>
2025-03-23 15:06:07 +0000

    feat: MySQLのDocker設定を更新し、初期化スクリプトと環境変数を追加。ユーザー権限を強化。

--- deploykit/db/sql/0_init_db.sql
+++ deploykit/db/sql/0_init_db.sql
@@ -1,13 +1,17 @@
 
 -- ユーザの作成 ※パスワード設定は必要に応じてこの処理のあとに変更する。
-SET GLOBAL validate_password.policy = 'LOW';
-ALTER USER root@localhost IDENTIFIED BY 'password';
-CREATE USER api@localhost IDENTIFIED BY 'password';
-CREATE USER dev@localhost IDENTIFIED BY 'password';
-FLUSH PRIVILEGES;
+SET GLOBAL validate_password_policy = 'LOW';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'api'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'dev'@'localhost' IDENTIFIED BY 'password';
+-- CREATE USER 'root'@'172.19.0.%' IDENTIFIED BY 'password';
+CREATE USER 'api'@'172.19.0.%' IDENTIFIED BY 'password';
+CREATE USER 'dev'@'172.19.0.%' IDENTIFIED BY 'password';
 
 -- 権限の付与
 GRANT SELECT,INSERT,UPDATE ON litter.* TO 'api'@'localhost';
 GRANT SELECT,INSERT,UPDATE ON litter.* TO 'api'@'172.19.0.%';
 GRANT SELECT,INSERT,UPDATE,DELETE,ALTER,CREATE ON litter.* TO 'dev'@'localhost';
 GRANT SELECT,INSERT,UPDATE,DELETE,ALTER,CREATE ON litter.* TO 'dev'@'172.19.0.%';
+
+FLUSH PRIVILEGES;
