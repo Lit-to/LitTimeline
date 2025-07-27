519091dd9d413fbb52cd1cbc7b5c1036a28e39f5
Lit-to <litto0315@gmail.com>
2025-07-27 17:25:38 +0900

    add:jsdocのlinterを追加

--- litter-app/eslint.config.ts
+++ litter-app/eslint.config.ts
@@ -1,5 +1,6 @@
 // eslint.config.js
 import js from "@eslint/js";
+import jsdocPlugin from "eslint-plugin-jsdoc";
 import prettierPlugin from "eslint-plugin-prettier";
 import prettierConfig from "eslint-config-prettier";
 
@@ -8,10 +9,25 @@ export default [
     prettierConfig,
     {
         plugins: {
+            jsdoc: jsdocPlugin,
             prettier: prettierPlugin
         },
         rules: {
-            "prettier/prettier": "error"
+            "prettier/prettier": "error",
+            "jsdoc/require-jsdoc": [
+                "error",
+                {
+                    require: {
+                        FunctionDeclaration: true,
+                        MethodDefinition: true,
+                        ClassDeclaration: true,
+                        ArrowFunctionExpression: false,
+                        FunctionExpression: false
+                    }
+                }
+            ],
+            "jsdoc/require-param": "error",
+            "jsdoc/require-returns": "error"
         }
     }
 ];
