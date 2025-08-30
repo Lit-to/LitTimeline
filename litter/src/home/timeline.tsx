9b6755e081f22390000bca1ab6faac138d2e0957
Lit-to <litto0315@gmail.com>
2025-08-30 17:07:13 +0900

    doc:TSDocを追記

--- litter/src/home/timeline.tsx
+++ litter/src/home/timeline.tsx
@@ -1,17 +1,33 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Card } from "react-bootstrap";
 import styles from "./timeline.module.css";
-import { useEffect, useRef, useState } from "react";
+import { JSX, useEffect, useRef, useState } from "react";
 import * as endPoint from "../endPoint.ts";
 import * as reactRouterDom from "react-router-dom";
+const POST_COUNT = Number(import.meta.env.VITE_POST_COUNT);
 
+/**
+ * 1ポストに必要なプロパティを表す型
+ * ※今後別ファイルに移動する可能性有、今回は簡易的に作成
+ *
+ * @typedef {PostCardProperties}
+ */
 type PostCardProperties = {
     title: string;
     name: string;
     content: string;
 };
 
-function PostCard({ title, name, content }: PostCardProperties) {
+/**
+ * 1ポストを表示するコンポーネント
+ * ※今後別ファイルに移動する可能性有、今回は簡易的に作成
+ * @param {PostCardProperties} param0
+ * @param {string} param0.title
+ * @param {string} param0.name
+ * @param {string} param0.content
+ * @returns {*}
+ */
+function PostCard({ title, name, content }: PostCardProperties): JSX.Element {
     return (
         <Card>
             <Card.Body>
@@ -23,45 +39,110 @@ function PostCard({ title, name, content }: PostCardProperties) {
     );
 }
 
-function CardFooter() {
-    return <div style={{ backgroundColor: "lightgray" }}>一番下のツイートです</div>;
+/**
+ * 一番下のポストを表示する番兵の型定義
+ */
+type CardFooterProps = {
+    footerRef: React.RefObject<HTMLDivElement | null>;
+};
+
+/**
+ * 一番下のポストを表示する関数
+ *
+ * @returns {*} - 一番下のポストを表示するコンポーネント
+ */
+function CardFooter({ footerRef }: CardFooterProps): JSX.Element {
+    return (
+        <div style={{ backgroundColor: "lightgray" }}>
+            <div ref={footerRef}>一番下のポストです</div>
+        </div>
+    );
 }
 
-function getPosts(id: number) {
+/**
+ * 1ポストを取得する関数
+ * ※今後別ファイルに移動する可能性有、今回は簡易的に作成
+ *
+ * @param {number} id - ポスト
+ID
+ * @returns {*} - 1ポストを表示するコンポーネント
+ */
+function getPosts(id: number): JSX.Element {
+    console.log(id);
     return <PostCard title={`post ${id}`} name={`user ${id}`} content={`content ${id}`}></PostCard>;
 }
-const POST_COUNT = 20;
 
+/**
+ * タイムラインフレームを表示する関数
+ *
+ * @param {{ children?: React.ReactNode }} param0
+ * @param {React.ReactNode} param0.children - フレーム内に表示する子要素
+ * @returns {JSX.Element}
+ */
+function Frame({ children }: { children?: React.ReactNode }): JSX.Element {
+    return (
+        <div className={styles.vertical}>
+            <div className={styles.card}>
+                <div className={styles.frameHeader}>Tlitter</div>
+                <div>{children}</div>
+            </div>
+        </div>
+    );
+}
+
+// 監視する領域（ブラウザの表示領域）
+/**
+ * ホーム画面を表示する関数
+ * サイドバー、ポストフレーム、ポスト一覧を含む
+ * @type {*} - ホーム画面を表示するコンポーネント
+ */
 function Home() {
-    // 監視する領域（ブラウザの表示領域）
-    const [, setLastPostId] = useState(0);
-    const [items, setItems] = useState<React.ReactNode[]>([]);
-    let footerRef = useRef<HTMLDivElement | null>(null);
+    const [, setLastPostId] = useState(0); // 最後に表示したポストのID
+    const [items, setItems] = useState<React.ReactNode[]>([]); // 表示するポストのリスト
+    let footerRef = useRef<HTMLDivElement | null>(null); // 一番下のポストを表示するための参照
     const options = {
         root: null,
     };
-    const observerObject = new IntersectionObserver(observeFook, options);
+    const observerObject = new IntersectionObserver(observeFook, options); // 画面位置監視オブジェクト
 
     useEffect(() => {
         createFook(footerRef);
         return () => observerObject.disconnect();
     }, []);
 
-    function createFook(footerRef: React.RefObject<HTMLDivElement | null>) {
+    /**
+     * 監視用フック発行関数
+     * 常にこの関数は実行され、footerRefの位置を監視する
+     * @param {React.RefObject<HTMLDivElement | null>} footerRef - 一番下のポストの参照
+     */
+    function createFook(footerRef: React.RefObject<HTMLDivElement | null>): void {
         if (footerRef.current != null) {
             observerObject.observe(footerRef.current);
         }
     }
 
-    function observeFook(entries: IntersectionObserverEntry[], observer: IntersectionObserver) {
+    /**
+     * 監視用フック
+     * footerRefのうち画面内にどの要素が入っているかを管理する
+     * @param {IntersectionObserverEntry[]} entries - footerRefの一覧
+     * @param {IntersectionObserver} observer - 監視オブジェクト
+     */
+    function observeFook(entries: IntersectionObserverEntry[], observer: IntersectionObserver): void {
+        //複数個が想定されているため0番目を指定している
         if (entries[0].isIntersecting) {
-            addCards(POST_COUNT);
+            addPosts(POST_COUNT);
         }
         observer.unobserve(entries[0].target);
         observer.observe(entries[0].target);
     }
 
-    function addCards(count: number) {
+    /**
+     * ポスト追加関数
+     * APIをたたき、ポストを追加する
+     * ※今後別ファイルに移動する可能性有、今回は簡易的に作成
+     * @param {number} count - 追加個数
+     */
+    function addPosts(count: number): void {
         setLastPostId((prev) => {
             let start = prev;
             for (let i = start + 1; i <= start + count; ++i) {
@@ -71,7 +152,12 @@ function Home() {
         });
     }
 
-    function addItem(item: React.ReactNode) {
+    /**
+     * 配列に要素を追加する関数
+     *
+     * @param {React.ReactNode} item - 追加する要素
+     */
+    function addItem(item: React.ReactNode): void {
         setItems((prev) => [...prev, item]);
     }
 
@@ -81,31 +167,33 @@ function Home() {
             <div className={styles.enableScroll}>
                 <Frame>
                     {items}
-                    <div ref={footerRef}>a</div>
-                    <CardFooter />
+                    <CardFooter footerRef={footerRef} />
                 </Frame>
             </div>
         </div>
     );
 }
 
+const navigate = reactRouterDom.useNavigate();
+
+/**
+ * ログアウトする関数
+ * APIをたたきセッションを削除する。
+ * @async
+ */
+async function logout(): Promise<void> {
+    await endPoint.logout(navigate);
+}
+
+/**
+ * サイドバー(ホームやログアウトのボタン)を置く関数
+ *
+ * @type {*} - サイドバーコンポーネント
+ */
 function SideBar() {
-    const [isOpen, setIsOpen] = useState(true);
-    function openSideBar() {
-        setIsOpen(!isOpen);
-    }
-
-    const navigate = reactRouterDom.useNavigate();
-
-    async function logout() {
-        await endPoint.logout(navigate);
-    }
-
     return (
         <div className={`${styles.sidebarPos} ${styles.sidebarStyle}`}>
-            <div className={styles.sidebarHeader} onClick={openSideBar}>
-                O
-            </div>
+            <div className={styles.sidebarHeader}>O</div>
             <ul>
                 <li className={styles.line}></li>
                 <li className={styles.line}>
@@ -120,17 +208,6 @@ function SideBar() {
                     </button>
                 </li>
             </ul>
-        </div>
-    );
-}
-
-function Frame({ children }: { children?: React.ReactNode }) {
-    return (
-        <div className={styles.vertical}>
-            <div className={styles.card}>
-                <div className={styles.frameHeader}>Tlitter</div>
-                <div>{children}</div>
-            </div>
         </div>
     );
 }
