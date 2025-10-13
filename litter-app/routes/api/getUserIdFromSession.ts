824f3206126576911ce60b9012acf29e2658d504
Lit-to <litto0315@gmail.com>
2025-10-13 23:22:32 +0900

    add:getUserIdを新セッション対応

--- litter-app/routes/api/getUserIdFromSession.ts
+++ litter-app/routes/api/getUserIdFromSession.ts
@@ -1,8 +1,22 @@
 import * as express from "express";
 const router = express.Router();
 import * as ResponseResult from "../../types/ResponseResult.ts";
-import { SessionHandler } from "../../types/SessionHandler.ts";
+import * as SessionManager from "../../types/SessionManager.ts";
 import * as constants from "../constants.ts";
+import * as common from "../common.ts";
+async function getUserIdFromSession(sessionId: string): Promise<ResponseResult.ResponseResult> {
+    const sessionManager = SessionManager.SessionManager.getInstance();
+    const sessionData = await sessionManager.getSessionFromSessionId(sessionId);
+    if (!sessionData.getIsSuccess) {
+        return;
+    }
+    let userId = sessionData.getResult[constants.SESSION_USER_ID];
+    if (userId == undefined) {
+        userId = constants.EMPTY_STRING;
+        sessionData[constants.SESSION_USER_ID] = userId;
+    }
+    return ResponseResult.ResponseResult.createSuccessWithData({ userId });
+}
 
 /**
  * セッションデータを返却するAPI
@@ -11,14 +25,19 @@ import * as constants from "../constants.ts";
  * @param {express.Response} res - レスポンスオブジェクト(自動挿入)
  * @returns {ResponseResult.ResponseResult} - 処理結果
  */
-function getUserIdFromSession(req: express.Request, res: express.Response): express.Response {
-    let userId = SessionHandler.getUserId(req.session);
-    if (userId == undefined) {
-        userId = constants.EMPTY_STRING;
-        SessionHandler.setUserId(req.session, constants.EMPTY_STRING);
+async function getUserIdFromSessionHandler(req: express.Request, res: express.Response): Promise<express.Response> {
+    // パラメータチェック
+    const allowedParams = [constants.PARAM_SESSION_ID];
+    const paramCheckResult = common.checkParameters(Object.keys(req.query), allowedParams);
+    if (!paramCheckResult.getIsSuccess) {
+        return paramCheckResult.formatResponse(res);
     }
-    return ResponseResult.ResponseResult.createSuccessWithData({ userId }).formatResponse(res);
+    // セッションIDの取得
+    const sessionId = req.query.sessionId as string;
+    // セッションデータの取得
+    const result = await getUserIdFromSession(sessionId);
+    return result.formatResponse(res);
 }
 
-router.get("/", getUserIdFromSession);
+router.get("/", getUserIdFromSessionHandler);
 export { router };
